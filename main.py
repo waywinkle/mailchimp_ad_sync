439cc52103e30df99e2de629e6377bb3c13a8ae0
JMiddleton <jabmiddleton@gmail.com>
2016-08-29 19:56:54 +1200

    getting the first and last name from both sides

--- main.py
+++ main.py
@@ -1,27 +1,18 @@
-import os
-import json
 from ad import get_ou_members
-from mailchimp import get_mailchimp_list_emails, update_mailchimp_list
-
-__location__ = os.path.realpath(
-    os.path.join(os.getcwd(), os.path.dirname(__file__)))
-
-
-def get_properties(file):
-    with open(os.path.join(__location__, file)) as json_file:
-        properties = json.load(json_file)
-
-    return properties
+from mailchimp import get_mailchimp_list_users, update_mailchimp_list
+from properties import get_all_properties
 
 
 def update_lists():
-    properties = get_properties('properties.json')
+    properties = get_all_properties('properties.json')
 
     for group, group_prop in properties['groups'].items():
         ad_list = get_ou_members(properties['global'], group_prop)
-        mc_list = get_mailchimp_list_emails(group_prop)
-        add_list = compare_lists(mc_list, ad_list)
-        delete_list = compare_lists(ad_list, mc_list)
+        mc_list = get_mailchimp_list_users(group_prop)
+        ad_email = [i['email'] for i in ad_list]
+        mc_email = [i['email'] for i in mc_list]
+        add_list = compare_lists(mc_email, ad_email)
+        delete_list = compare_lists(ad_email, mc_email)
         update_mailchimp_list(group_prop, add_list, 'ADD')
         update_mailchimp_list(group_prop, delete_list, 'DELETE')
 
@@ -39,6 +30,10 @@ def compare_lists(base, new):
             delta.append(member.lower())
 
     return delta
+
+
+def get_updates(ad_list, mc_list):
+    pass
 
 
 if __name__ == '__main__':
